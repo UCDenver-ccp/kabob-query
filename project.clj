239a2d72051fe45dba421b2b0b2b5926ec9af479
Marc Daya <marc@sinistral.net>
2016-04-12 15:46:47 -0600

    Remove defunct artefacts post template repo merge.
    Neither the template build files, nor the dependency on the templates
    package are needed any longer.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 
 (defproject edu.ucdenver.ccp/kabob-query "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A canned query interface for the Knowledgebase of Biomedicine."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
@@ -11,7 +10,6 @@
                  [stencil "0.5.0"]
                  [edu.ucdenver.ccp/kabob-build "1.3.0-SNAPSHOT"
                   :exclusions [org.slf4j/slf4j-log4j12 potemkin]]
-                 [edu.ucdenver.ccp/kabob-query-templates "0.2.0-SNAPSHOT"]
                  [edu.ucdenver.ccp/kr-core "1.4.20-SNAPSHOT"]
                  [edu.ucdenver.ccp/kr-sesame-core "1.4.20-SNAPSHOT"]]
 
