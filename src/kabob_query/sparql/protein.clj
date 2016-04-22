144f1e5ef6457bbe50e4a2f245ce0e65faf949a8
Marc Daya <marc@sinistral.net>
2016-04-22 14:55:24 -0600

    Return ext IDs in the protein interaction query.
    This first attempt at using the machinery for mapping a BIO ID into an
    external ID (based on the IDs used by the user as input) is particularly
    expensive as it requires an additional trip to the store *FOR EACH* BIO
    ID in the result set.
    
    This inefficiency will be address in a future commit.

--- src/kabob_query/sparql/protein.clj
+++ src/kabob_query/sparql/protein.clj
@@ -37,9 +37,24 @@
 
 (define-interface-fn binary-interaction-partners kb
   [source-id]
-  (let [ice-id (id/id->ice-uri source-id (:iao-namespaces kb))]
-    (sparql-query kb
-                  (render "sparql/protein/binary-interaction-partners"
-                          {:src-id source-id
-                           :ice-id ice-id
-                           :bio-id (id/bio-id kb ice-id)}))))
+  (let [ice-uri (id/id->ice-uri source-id (:iao-namespaces kb))
+        [iao eid] (id/ext-id->parts source-id)
+        bio->ext (fn [id]
+                   (let [fqbn (id/fq kb id)
+                         enid (id/ice-id kb :protein iao fqbn)]
+                     (id/parts->ext-id iao enid)))]
+    ;; We enrich the resultset that is returned with our best guess at the
+    ;; 'primary' external ID for the partner proteins.  Unfortunately, this is
+    ;; quite and expensive operation, since it requires us to go back to the
+    ;; store to retrieve all of the source-specific IDs that may denote the BIO
+    ;; entity.
+    ;;
+    ;; Ideally, we'd want to group by the `partner_bio_id` and use something
+    ;; like MySQL's `group_concat` to collapse the all of the records for each
+    ;; partner ID into a single row.
+    (map #(assoc % '?/ext_partner_id (bio->ext (get % '?/partner_bio_id)))
+         (sparql-query kb
+                       (render "sparql/protein/binary-interaction-partners"
+                               {:src-id source-id
+                                :ice-id ice-uri
+                                :bio-id (id/bio-id kb ice-uri)})))))
