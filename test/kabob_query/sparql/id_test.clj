5ce937a96bf8fb2823904f7a5035b69ba203ea9c
Marc Daya <marc@sinistral.net>
2016-04-27 10:51:49 -0600

    Post-merge test clean-up.
    * Remove defunct tests for functions that no longer exist: `ice-id1`.
    * Move ID tests in to the correct namespace.

--- test/kabob_query/sparql/id_test.clj
+++ test/kabob_query/sparql/id_test.clj
@@ -9,11 +9,28 @@
   => '[{?/id kbio/BIO_123}]
   (provided (sparql-query ..kb.. anything) => '[{?/id kbio/BIO_123}]))
 
+(fact
+  (#'id/bio-id {:ns-map-to-long {"kbio" "http://kabob/bio/"}} ..ice-id..)
+  => "http://kabob/bio/BIO_123"
+  (provided (#'id/query:bioentity anything anything)
+            => '[{?/id kbio/BIO_123}]))
+
 (facts
   (fact (id/ice-id->entity-id "FOO_P123_ICE") => "P123")
   (fact (id/ice-id->entity-id "FOO_P_123_ICE") => "P_123"))
 
+(facts
+  (fact
+    (#'id/id->ice-uri "uniprot:p0" {"iaouniprot" "http://kabob/iao/uniprot/"})
+    => "http://kabob/iao/uniprot/UNIPROT_P0_ICE")
+  (fact
+    (#'id/id->ice-uri "uniprot:p0" {})
+    => (throws #"'uniprot' is not a valid IAO")))
 
 (facts
-  (fact (id/ice-uri->id "http://kabob.ucdenver.edu/iao/uniprot/UNIPROT_A0A0G2KB10_ICE") => "uniprot:A0A0G2KB10")
-  (fact (id/ice-uri->id "http://kabob.ucdenver.edu/iao/refseq/REFSEQ_XP_005158717_ICE") => "refseq:XP_005158717"))
+  (fact
+    (id/ice-uri->id "http://kabob.ucdenver.edu/iao/uniprot/UNIPROT_A0A0G2KB10_ICE")
+    => "uniprot:A0A0G2KB10")
+  (fact
+    (id/ice-uri->id "http://kabob.ucdenver.edu/iao/refseq/REFSEQ_XP_005158717_ICE")
+    => "refseq:XP_005158717"))
--- test/kabob_query/sparql/protein_test.clj
+++ test/kabob_query/sparql/protein_test.clj
@@ -6,20 +6,6 @@
             [kabob-query.test.util :refer [*result* collect]])
   (:use [midje.sweet]))
 
-(fact
-  (#'id/id->ice-uri "uniprot:p0" {"iaouniprot" "http://kabob/iao/uniprot/"})
-  => "http://kabob/iao/uniprot/UNIPROT_P0_ICE")
-
-(fact
-  (#'id/id->ice-uri "uniprot:p0" {})
-  => (throws #"'uniprot' is not a valid IAO"))
-
-(fact
-  (#'id/bio-id {:ns-map-to-long {"kbio" "http://kabob/bio/"}} ..ice-id..)
-  => "http://kabob/bio/BIO_123"
-  (provided (#'id/query:bioentity anything anything)
-            => '[{?/id kbio/BIO_123}]))
-
 (binding [*result* (atom [])]
   (facts
     (fact (query "sparql/protein/cellular-components" ["uniprot:p123"] {} collect)
@@ -39,19 +25,3 @@
     (against-background
       (#'kabob-query.kb/open-kb-impl {}) => (open (kb :sesame-mem))
       (#'id/query:bioentity anything anything) => '[{?/id kbio/BIO_123}])))
-
-(facts "The 'highest' lexographically sorted ID is used as the primary ID."
-  (fact "uniprot"
-    (#'id/ice-id1 "uniprot" ["UNIPROT_P01_ICE" "UNIPROT_P09_ICE" "UNIPROT_P03_ICE"])
-    => "P09"))
-
-(facts "UniProt IDs that start with 'P' are preferred."
-  (fact "the only P-id is selected"
-    (#'id/ice-id1 "uniprot" ["UNIPROT_Q1_ICE" "UNIPROT_P1_ICE" "UNIPROT_R1_ICE"])
-    => "P1")
-  (fact "the 'highest' P-id is selected"
-    (#'id/ice-id1 "uniprot" ["UNIPROT_P9_ICE" "UNIPROT_P1_ICE" "UNIPROT_R1_ICE"])
-    => "P9")
-  (fact "in the absence of a P-id, the 'highest' ID is selected."
-    (#'id/ice-id1 "uniprot" ["UNIPROT_Q1_ICE" "UNIPROT_X1_ICE" "UNIPROT_R1_ICE"])
-    => "X1"))
